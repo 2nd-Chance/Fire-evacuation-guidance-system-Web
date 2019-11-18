b490dd31edb750ed4dd2a3149f7bb6a802f6c5c7
myulchkun <steven800@naver.com>
2019-11-18 14:24:29 +0900

    UI implementation complete
    UI implementation complete
    inner implementations to function UI are needed

--- static/js/Control.js
+++ static/js/Control.js
@@ -4,9 +4,11 @@ class Control {
         if(this.getAlert()){
             document.getElementById("home_text").innerHTML = "Fire alert! <br> Please evacuate";
             document.getElementById("Home").style.backgroundColor = "#FFABAB";
+            document.getElementById("home_image").src = "../static/image/running_black.png";
         }else{
             document.getElementById("home_text").innerHTML = "There is no fire. <br> You are safe.";
             document.getElementById("Home").style.backgroundColor = "white";
+            document.getElementById("home_image").src = "../static/image/standing_black.png";
         }
     }
 
