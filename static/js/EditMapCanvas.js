d7d63aafa17c2b8d83f70a807a335f1c9b49991c
myulchkun <steven800@naver.com>
2019-11-21 12:15:41 +0900

    Evacuation Route canvas
    Evacuation Route canvas
    draw map for user
    bellman ford algorithm implement is needed

--- static/js/EditMapCanvas.js
+++ static/js/EditMapCanvas.js
@@ -1,36 +1,42 @@
 class EditMapCanvas {
-
-    constructor(canvas, size) {
-        this._canvas = document.querySelector("Edit_Map_canvas");
-        //document.querySelector("room#room1").getAttribute("alertState")
-        this.initialize();
-        this.ctx = document.getElementById('Edit_Map_canvas').getContext("2d");
+    constructor() {
+        this.canvas = document.getElementById("Edit_Map_canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.nodeImg = new Image();
+        this.nodeImg.src = "../static/image/node_black.png";
         this.penMode = false;
+        this.isDrag = false;
+        this.WIDTH = 400;
+        this.HEIGHT = 400;
+        this.room = [];
+        // this.ctx.onmousedown = this.mouseDown();
+        // this.ctx.onmouseup = this.mouseUp();
+
+        //this.room은 캔버스에 올라온 노드들의 목록
+        //room은 모든 노드들의 목록
+    }
+
+    getRoomByID(roomId) {
+        return this.room[roomId];
     }
 
     drawTrashCan(){
         let trashCan = new Image();
-        trashCan.src = "../static/image/trash_bin_black.png";
+        trashCan.src = "./static/image/trash_bin_black.png";
         this.ctx.drawImage(trashCan, 200, 200, 32, 32);
-    }
-
-    initialize() {
-
-    }
-
-    getRoomByID(roomId) {
-        return this.room[id];
     }
 
     changeMode(){
         this.penMode = !this.penMode;
+        alert(this.penMode);
+    }
+
+    saveMap() {
+        alert('save function not implemented');
     }
 
     addRoomToCanvas(roomId){
-        let imgClo = new Image();
         let room = document.querySelector('#roomList room[roomId = "' +  roomId + '"]');
-
-        imgClo.src = "../static/image/node_black.png";
 
         this.ctx.textBaseline = 'top';
         this.ctx.font='13px Verdana';
@@ -38,29 +44,64 @@ class EditMapCanvas {
         this.ctx.textAlign = 'center';
         this.ctx.fillText(roomId, 55, 95);
 
+        room.setAttribute('level', 1);
+
         room.setAttribute('X',30);
         room.setAttribute('Y', 30);
+        this.room[roomId].X = 30;
+        this.room[roomId].Y = 30;
+
         document.querySelector('#freeNodeList room[roomId = "' + roomId + '"]').remove();
 
-        this.ctx.drawImage(imgClo, 30, 30, 50, 50);
+        this.ctx.drawImage(this.nodeImg, 30, 30, 50, 50);
     }
 
     deleteNode(roomId) {
-
+        let room = document.querySelector('#roomList room[roomId = "' +  roomId + '"]');
+        room.setAttribute('level', 0);
+        this.room.remove(roomId);
     }
 
     selectNodeById(roomId) {
         this.currentNode = document.querySelector('#roomList room[roomId = "' + roomId + '"]');
-        this.currentNode.getAttribute('X')
+        this.currentNode.getAttribute('X');
+        this.currentNode.getAttribute('Y');
     }
 
-    canvasEvent() {
-        var x = event.offsetX;
-        var y = event.offsetY;
-
+    clear(){
+        this.ctx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+    }
+/*
+    mouseMove(e) {
+        if(this.isDrag){
+            this.mouseX = e.pageX - this.canvas.offsetLeft;
+            this.mouseY = e.pageY - this.canvas.offsetTop;
+            this.drawNode();
+        }
     }
 
-    saveMap(){
-
+    drawNode() {
+        this.ctx.drawImage(this.nodeImg, this.mouseX - 25, this.mouseY - 25, 50, 50);
     }
+
+    mouseDown(e){
+        for(let i = 0; i < this.room.length; i++) {
+            if(this.room[i] == null) i++;
+            if(e.pageX < this.room[i].X + 25 + this.canvas.offsetLeft && e.pageX < this.room[i].X - 25 + this.canvas.offsetLeft
+            && e.pageY < this.room[i].Y + 25  + this.canvas.offsetTop && e.pageY < this.room[i].Y - 25 + this.canvas.offsetTop) {
+                this.mouseX = e.pageX - this.ctx.offsetLeft;
+                this.mouseY = e.pageY - this.ctx.offsetTop;
+                this.isDrag = true;
+                this.canvas.onmousemove = this.mouseMove();
+            }
+        }
+        alert("mouse down");
+    }
+
+    mouseUp() {
+        this.isDrag = false;
+        this.canvas.onmousemove = null;
+        alert("mouse up");
+    }
+    */
 }
--- static/js/EvacuationRouteCanvas.js
+++ static/js/EvacuationRouteCanvas.js
@@ -1,7 +1,93 @@
 class EvacuationRouteCanvas {
     constructor() {
-        let control = new Control();
+        this.blackNode = new Image();
+        this.redNode = new Image();
+        this.greenNode = new Image();
+        this.blackNode.src = "../static/image/node_black.png";
+        this.redNode.src = "../static/image/node_red.png";
+        this.greenNode.src = "../static/image/node_green.png";
+        this.nodeImgWidth = 50;
+        this.nodeImgHeight = 50;
 
+        this.canvas = document.getElementById("Evacuation_Route_canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.room = document.querySelectorAll("#roomList room");
+
+        setInterval(this.checkAlert,10000);
+    }
+
+    checkAlert() {
+        control.setAlert(false);
+        for(let i = 0; this.room.length; i++){
+            if(this.room[i].getAttribute("alertState") === true){
+                control.setAlert(true);
+            }
+        }
+        this.clear();
+        this.drawMap();
+    }
+
+    clear() {
+        this.canvas.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    checkNodeState(i) {
+        if(this.room[i].getAttribute("alertState") === true){
+            return "red";
+        } else if(this.room[i].getAttribute("alertState") === false
+            && this.room[i].getAttribute("route") === "green"){
+            return "green";
+        } else{
+            return "black";
+        }
+    }
+
+    drawMap(){
+        for(let i = 0; this.room.length; i++){
+            let link = document.querySelectorAll("#roomList room[roomId = " + this.room[i].getAttribute("roomId") + "] linkTo");
+            for(let j = 0; j < link.length; j++){
+                this.drawEdge(this.room[i].getAttribute("X"), this.room[i].getAttribute("Y"),
+                    document.querySelector("#roomList room[roomId = "+ link[j].value + "]").getAttribute("X"),
+                    document.querySelector("#roomList room[roomId = "+ link[j].value + "]").getAttribute("Y"));
+            }
+            this.drawNode(this.room[i].getAttribute("name"), this.room[i].getAttribute("X"),
+                this.room[i].getAttribute("Y"), this.checkNodeState(i));
+        }
+    }
+
+    drawEdge(startX, startY, endX, endY, color) {
+        if(color === null || color ==="black"){
+            this.ctx.beginPath();
+            this.ctx.moveTo(startX, startY);
+            this.ctx.lineTo(endX, endY);
+            this.ctx.strokeStyle = "black";
+            this.ctx.stroke();
+        }else if(color ==="green") {
+            this.ctx.beginPath();
+            this.ctx.moveTo(startX, startY);
+            this.ctx.lineTo(endX, endY);
+            this.ctx.strokeStyle = "green";
+            this.ctx.stroke();
+        }
+    }
+
+    drawNode(name, X, Y, color){
+        switch (color) {
+            case "black":
+                this.ctx.drawImage(this.blackNode, X, Y, this.nodeImgWidth, this.nodeImgHeight);
+                break;
+            case "red":
+                this.ctx.drawImage(this.redNode, X, Y, this.nodeImgWidth, this.nodeImgHeight);
+                break;
+            case "green":
+                this.ctx.drawImage(this.greenNode, X, Y, this.nodeImgWidth, this.nodeImgHeight);
+                break;
+        }
+        this.ctx.textBaseline = 'top';
+        this.ctx.font='13px Verdana';
+        this.ctx.fillStyle = 'black';
+        this.ctx.textAlign = 'center';
+        this.ctx.fillText(name, X, Y);
     }
 
 
